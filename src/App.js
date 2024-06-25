a0985201edb46ba991ab5ab2ed04559a072aa216
Santiago Rodriguez <santiagorrodriguez9@gmail.com>
2024-06-24 23:16:43 -0500

    Primeros cambios

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import joel from './imagenes/joel.jpeg';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='logo-contenedor'>
+        <img 
+        className='logo' 
+        src={joel} alt='logo' >
+        </img>
+      </div>
+      <div className='contenedor-principal'>
+        
+      </div>
     </div>
   );
 }
